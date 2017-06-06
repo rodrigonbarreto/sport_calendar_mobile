d60db7c95471eb2f8cff229c473303a93d930eee
Rodrigo Barreto <rodrigo.barreto@mall-connect.com>
2017-06-06 14:32:58 +0200

    list exercises

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+
 
 @NgModule({
   declarations: [
@@ -21,6 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) {
+  public todayList;
+  public lastWeekList;
+  public twoWeeksAgo;
 
+  constructor(
+        public navCtrl: NavController, 
+        private _http: 
+        Http,private _loadingCtrl: 
+        LoadingController, 
+        private _alertCtrl: AlertController
+    ) {}
+
+  ngOnInit() {
+
+      let loader = this._loadingCtrl.create({
+        content: 'Buscando novos todayList. Aguarde ...'
+      });
+      
+      loader.present();
+
+      this._http
+        .get('http://172.16.0.5:8080/en/exercise/list')
+        .map(res => res.json())
+        .toPromise()
+        .then(todayList => {
+          this.todayList = todayList['today'];
+          this.lastWeekList = todayList['week_ago'];
+          this.twoWeeksAgo = todayList['two_week_ago']
+          console.log(this.todayList);
+          
+          loader.dismiss();
+        })
+        .catch(err => {
+          console.log(err);
+          loader.dismiss();
+          this._alertCtrl
+            .create({
+              title: 'Falha na conexão',
+              buttons: [{ text: 'Estou ciente!'}],
+              subTitle: 'Não foi possível obter a lista de carros. Tente mais tarde.'
+            }).present();
+        });
   }
 
 }
